a58918206044d4254d3a6dbef4062602f44a3e28
SneedsSeedAndFeed <bouncingwaffle@gmail.com>
2021-12-12 15:32:02 +1100

    wip health

---
+++ Assets/UI Elements/Health Bar/HealthManager.cs
@@ -0,0 +1,96 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthManager : MonoBehaviour
+{
+    public Text healthText;
+    public Image healthBar;
+    
+    float health , maxHealth = 100;
+
+    float headMaxHealth = 70;
+    float torsoMaxHealth = 100;
+    float left_ArmMaxHealth , right_ArmMaxHealth = 60;
+    float left_legMaxHealth , right_legMaxHealth = 80;
+
+
+
+    float changeSpeed;
+    
+    private void Start()
+    {
+
+        health = maxHealth;
+        defineThings();
+
+
+    }
+
+    private void Update()
+    {
+
+        healthText.text = "Health: " + health + "%";
+        if ( health > maxHealth ) health = maxHealth;
+
+        changeSpeed = 5 * Time.deltaTime;
+
+        HealthBarFiller();
+        colourChanger();
+        
+
+    }
+    
+
+    static void defineThings()
+    {
+        var bodyParts = new List<KeyValuePair< string , int >>();
+        bodyParts.Add(new KeyValuePair< string , int >("head" , 70 ));
+        bodyParts.Add(new KeyValuePair< string , int >("torsoHealth" , 100 ));
+        bodyParts.Add(new KeyValuePair< string , int >("left_ArmHealth" , 60 ));
+        bodyParts.Add(new KeyValuePair< string , int >("right_ArmHealth" , 60 ));
+        bodyParts.Add(new KeyValuePair< string , int >("left_legHealth" , 80 ));
+        bodyParts.Add(new KeyValuePair< string , int >("right_legHealth" , 80 ));
+
+    }
+
+    void HealthBarFiller()
+    {
+
+        healthBar.fillAmount = Mathf.Lerp( healthBar.fillAmount , health / maxHealth , changeSpeed );
+        
+    }
+
+    public void decreaseHealth( string inputs )
+    {
+        
+        string temp = inputs.Substring( 0 , 3 );
+        int damage = int.Parse( temp );
+        int ind = inputs.LastIndexOf( ':' );
+        temp = inputs.Substring(ind + 1);
+        string bodyPart = temp;
+
+        var random = new System.Random();
+        int randVal = random.Next(bodyParts.Count);
+        Debug.Log( bodyParts[randVal] );
+
+
+    }
+    
+    public void increaseHealth( float increasedHealth )
+    {
+
+        if ( health < maxHealth )
+            health += increasedHealth; 
+
+    }
+
+    void colourChanger()
+    {
+
+        Color healthColour = Color.Lerp( Color.red , Color.green , ( health / maxHealth ));
+        healthBar.color = healthColour;
+
+    }
+}
