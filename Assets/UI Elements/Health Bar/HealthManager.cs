79c53e8d17e3cbbf97292e7646d59428223175a5
SneedsSeedAndFeed <bouncingwaffle@gmail.com>
2021-12-13 14:25:24 +1100

    fixing github messup

--- Assets/UI Elements/Health Bar/HealthManager.cs
+++ Assets/UI Elements/Health Bar/HealthManager.cs
@@ -2,14 +2,7 @@ using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-=======
 using System.Linq;
->>>>>>> Stashed changes
-=======
-using System.Linq;
->>>>>>> Stashed changes
 
 public class HealthManager : MonoBehaviour
 {
@@ -24,17 +17,6 @@ public class HealthManager : MonoBehaviour
     
     float health , maxHealth = 100;
 
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-    float headMaxHealth = 70;
-    float torsoMaxHealth = 100;
-    float left_ArmMaxHealth , right_ArmMaxHealth = 60;
-    float left_legMaxHealth , right_legMaxHealth = 80;
-
-    List<KeyValuePair<string, int>> bodyParts = new List<KeyValuePair<string, int>>();
-=======
-=======
->>>>>>> Stashed changes
     string bodyPart = "headHealth";
 
     float headMaxHealth = 70;
@@ -45,10 +27,6 @@ public class HealthManager : MonoBehaviour
     Dictionary<string, int> bodyParts = new Dictionary<string, int>(); 
     //ICollection<KeyValuePair<string, int>> bodyParts = new Dictionary<string, int>();
     
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
 
     float changeSpeed;
     
@@ -78,51 +56,20 @@ public class HealthManager : MonoBehaviour
 
     void defineThings()
     {
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-        bodyParts.Add(new KeyValuePair< string , int >("head" , 70 ));
-        bodyParts.Add(new KeyValuePair< string , int >("torsoHealth" , 100 ));
-        bodyParts.Add(new KeyValuePair< string , int >("left_ArmHealth" , 60 ));
-        bodyParts.Add(new KeyValuePair< string , int >("right_ArmHealth" , 60 ));
-        bodyParts.Add(new KeyValuePair< string , int >("left_legHealth" , 80 ));
-        bodyParts.Add(new KeyValuePair< string , int >("right_legHealth" , 80 ));
-=======
-=======
->>>>>>> Stashed changes
         bodyParts.Add("headHealth" , 70 );
         bodyParts.Add("torsoHealth" , 100 );
         bodyParts.Add("left_ArmHealth" , 60 );
         bodyParts.Add("right_ArmHealth" , 60 );
         bodyParts.Add("left_LegHealth" , 80 );
         bodyParts.Add("right_LegHealth" , 80 );
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-
     }
 
     void HealthBarFiller()
     {
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-        Debug.Log( bodyParts[1] );
         headHealth.fillAmount = Mathf.Lerp( headHealth.fillAmount , health / maxHealth , changeSpeed );
         
     }
 
-    public void decreaseHealth( string inputs )
-    {
-        var result = bodyParts.Where(kvp => kvp.Value == "head");
-        Debug.Log( result );
-        Debug.Log( bodyParts[1] );
-=======
-=======
->>>>>>> Stashed changes
-        //Debug.Log( bodyParts[1] );
-        headHealth.fillAmount = Mathf.Lerp( headHealth.fillAmount , bodyParts[bodyPart] / headMaxHealth , changeSpeed );
-        
-    }
 
     static string determinePart(string value)
     {
@@ -147,33 +94,17 @@ public class HealthManager : MonoBehaviour
     {
   
         //Debug.Log( bodyParts[1] );
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
         string temp = inputs.Substring( 0 , 3 );
         int damage = int.Parse( temp );
         int ind = inputs.LastIndexOf( ':' );
         temp = inputs.Substring(ind + 1);
         string bodyPart = temp;
 
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-        var random = new System.Random();
-        int randVal = random.Next(bodyParts.Count);
-        Debug.Log( bodyParts[randVal] );
-=======
-=======
->>>>>>> Stashed changes
         Debug.Log(determinePart(bodyPart));
         
         Debug.Log(bodyParts[bodyPart]);
         if ( bodyParts[bodyPart] > 0 )
             bodyParts[bodyPart] -= damage;
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
 
 
     }
@@ -189,15 +120,8 @@ public class HealthManager : MonoBehaviour
     void colourChanger()
     {
 
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-        Color healthColour = Color.Lerp( Color.red , Color.green , ( health / maxHealth ));
-=======
         Color healthColour = Color.Lerp( Color.red , Color.green , ( bodyParts[bodyPart] / headMaxHealth ));
->>>>>>> Stashed changes
-=======
-        Color healthColour = Color.Lerp( Color.red , Color.green , ( bodyParts[bodyPart] / headMaxHealth ));
->>>>>>> Stashed changes
+
         headHealth.color = healthColour;
 
     }
