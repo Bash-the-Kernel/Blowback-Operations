d556e860c1e61c0a004917fa134bb82f5e6712e4
Jack <0319024@schoolsnet.act.edu.au>
2021-12-29 12:36:01 +1100

    enemy that is essentially an aimbot

--- Assets/scripts/enemy_control.cs
+++ Assets/scripts/enemy_control.cs
@@ -5,21 +5,33 @@ using UnityEngine;
 public class enemy_control : MonoBehaviour
 {
     public GameObject bullet;
+    public float rotSpeed;
+    public float moveSpeed;
+    public GameObject player;
+    public float time = 0f;
+    public Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.Find("player");
     }
-
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         action_control();
     }
 
     void action_control()
     {
-
+        if (can_See(player))
+        {
+            StartCoroutine(RotateTowards(0.2f, player.transform.position));
+            if (Time.time - time >= 0.3f)
+            {
+                StartCoroutine(shoot(0.1f));
+                time = Time.time;
+            }
+        }
     }
 
     void MoveToPos(Vector3 position)
@@ -27,26 +39,113 @@ public class enemy_control : MonoBehaviour
 
     }
 
-    IEnumerator Wait(float time)
+    bool can_See(GameObject target)
     {
-        yield return new WaitForSeconds(time);
+        Vector3 direction = new Vector3(target.transform.position.x - transform.position.x, 0, target.transform.position.z - transform.position.z);
+
+        //now make a rotation value that can be attributed to the enemy such that it is facing the player
+        float angle = Mathf.Atan2(direction.z, direction.x) * Mathf.Rad2Deg - 90;
+        Quaternion target_rot = Quaternion.AngleAxis(angle, Vector3.forward);
+
+        //these are masks that will be used such that the ray that will be cast only hits the player and not other things like walls and other enemies
+        int mask = 1 << LayerMask.NameToLayer("player");
+        mask |= 1 << LayerMask.NameToLayer("Default");
+
+        Vector3 startpos = transform.position;
+        startpos.y = 1;
+        RaycastHit hit;
+        if (Physics.Raycast(startpos, direction, out hit, Mathf.Infinity))
+        {
+
+            Debug.DrawRay(startpos, direction * hit.distance, Color.yellow);
+            //Debug.Log("Did Hit");
+            //print(Vector3.Angle(direction, transform.right));
+            if (Vector3.Angle(direction, transform.right) < 80 && hit.collider.tag == target.tag)
+            {
+                //print("KIZZMADICK");
+                //if the player is within the enemies field of vision which is 80 degrees
+                //then do the running towards the player coroutine
+                Debug.DrawRay(transform.position, direction * hit.distance, Color.white);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else
+        {
+            return false;
+        }
     }
 
-
-    void shoot()
+    Vector3 leading_shot_pos(Vector3 target, Rigidbody target_rb)
     {
+        Vector3 position;
+        float time_est = (Vector3.Distance(target, transform.position) * player.GetComponent<player_control>().moveSpeed * 1.28f / bullet.GetComponent<bullet_controller_>().bullet_speed);
+        print(time_est);
+        if (target_rb.velocity.magnitude > 0)
+        {
+            position = target + target_rb.velocity * time_est;
+
+        }
+        else
+        {
+            position = target;
+        }
+        print(target.x + " " + target.y + " " + target.z);
+        print(" ");
+        print(position.x + " " + position.y + " " + position.z);
+        return position;
+
+
+    }
+
+    IEnumerator MoveForward(float time)
+    {
+        yield return new WaitForSeconds(time);
+        transform.position += transform.right * moveSpeed * Time.deltaTime;
+    }
+    
+
+
+    IEnumerator shoot(float time)
+    {
+        //print("LIGMABALLS");
+        yield return new WaitForSeconds(time);
         Quaternion bullet_rot = transform.rotation;
-        Vector3 bullet_pos = GameObject.Find("firepoint").transform.position;
+        Vector3 bullet_pos = GameObject.Find("enemy_firepoint").transform.position;
         GameObject bullet_parent = GameObject.Find("enemy_parent");
 
         bullet_pos.y = 1;
-        print(transform.rotation.eulerAngles.x);
-        bullet_rot.eulerAngles.Set(90, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z);
+        //print(transform.rotation.eulerAngles.x);
+        //bullet_rot.eulerAngles.Set(90, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z);
         Instantiate(bullet, bullet_pos, bullet_rot, bullet_parent.transform);
+
     }
 
-    void RotateTowards(Vector3 position)
+    IEnumerator RotateTowards(float time, Vector3 position)
     {
+        
+        if (position == player.transform.position)
+        {
+            yield return new WaitForSeconds(time);
+            Vector3 lead_pos = leading_shot_pos(player.transform.position, player.GetComponent<player_control>().rb);
+            //print(lead_pos.x + " " + player.transform.position.x);
+            Vector3 direction = new Vector3(lead_pos.x - transform.position.x, 0,lead_pos.z - transform.position.z);
+            float angle = Mathf.Atan2(direction.z, direction.x) * Mathf.Rad2Deg;
+            Quaternion target_rot = Quaternion.Euler(90f, 0f, angle);
+            transform.rotation = Quaternion.Slerp(transform.rotation, target_rot, rotSpeed * Time.deltaTime);
+        }
+        else
+        {
+            yield return new WaitForSeconds(time);
+            Vector3 direction = new Vector3(player.transform.position.x - transform.position.x, 0, player.transform.position.z - transform.position.z);
+            float angle = Mathf.Atan2(direction.z, direction.x) * Mathf.Rad2Deg;
+            Quaternion target_rot = Quaternion.Euler(90f, 0f, angle);
+            transform.rotation = Quaternion.Slerp(transform.rotation, target_rot, rotSpeed * Time.deltaTime);
+
+        }
 
     }
 
